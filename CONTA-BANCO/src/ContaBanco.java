9782e7f9b15eab0aad98e5d9bee740751c37cdc1
¨Sagat¨ <realsagatx@gmail.com>
2024-05-25 14:04:18 -0300

    Projeto pronto

--- CONTA-BANCO/src/ContaBanco.java
+++ CONTA-BANCO/src/ContaBanco.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu numero");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite sua agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", Obrigado por criar uma conta em nosso banco");
+        System.out.println("sua agência é " + agencia + " Conta "  + numero + " e seu Saldo " + saldo + " já está disponível para saque.");
+
+
+    }   
 }
