7e605d48133a6ce3d615b9258f3e5441a213cd64
¨Sagat¨ <realsagatx@gmail.com>
2024-06-09 03:25:10 -0300

    Aula concluida

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,9 @@
 public class ResultadoEscolar {
-    public static void main(String[] args) {
-        int nota = 4;
+	public static void main(String[] args) {
+		int nota = 5;
 
-        if(nota >=7)
-        System.out.println("Aprovado");
-
-        else if (nota >5 && nota <7) 
-        System.out.println("Prova de Recuperação");
-        
-        else
-        System.out.println("Reprovado");
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		
+        System.out.println(resultado);
         }
 }
