22bee846124fd7c9fd3d823c037f4d2bcb3d8aa6
¨Sagat¨ <realsagatx@gmail.com>
2024-06-15 08:46:08 -0300

    novo commit

---
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
+        
+        for (int x=0; x<alunos.length; x++)
+           
+            System.out.println("O aluno indice x=" + x + " é " + alunos[x]);
+        }
+        
+    }
---
+++ controle-de-fluxo/src/Exemplofor.java
@@ -0,0 +1,7 @@
+public class Exemplofor {
+	public static void main(String[] args) {
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+	}
+}
